0c496d21ff16342169d2f43a340a10e9229e6de6
Flavio Akira Nakahara <flavio.nakahara@gmail.com>
2015-07-09 16:43:05 -0300

    Revert "Algumas alterações no banco de dados"
    This reverts commit dd2cb3578fcb8cc7b43ae7d3ccdd443450583f39.

---
+++ ProjetoPratico/ProjetoPratico/Controllers/AtividadesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProjetoPratico.Models;
+
+namespace ProjetoPratico.Controllers
+{
+    public class AtividadesController : Controller
+    {
+        private BancoEntities db = new BancoEntities();
+
+        // GET: Atividades
+        public ActionResult Index()
+        {
+            return View(db.Atividade.ToList());
+        }
+
+        // GET: Atividades/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Atividade atividade = db.Atividade.Find(id);
+            if (atividade == null)
+            {
+                return HttpNotFound();
+            }
+            return View(atividade);
+        }
+
+        // GET: Atividades/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Atividades/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,descricao,prazo,id_projeto")] Atividade atividade)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Atividade.Add(atividade);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(atividade);
+        }
+
+        // GET: Atividades/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Atividade atividade = db.Atividade.Find(id);
+            if (atividade == null)
+            {
+                return HttpNotFound();
+            }
+            return View(atividade);
+        }
+
+        // POST: Atividades/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,descricao,prazo,id_projeto")] Atividade atividade)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(atividade).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(atividade);
+        }
+
+        // GET: Atividades/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Atividade atividade = db.Atividade.Find(id);
+            if (atividade == null)
+            {
+                return HttpNotFound();
+            }
+            return View(atividade);
+        }
+
+        // POST: Atividades/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Atividade atividade = db.Atividade.Find(id);
+            db.Atividade.Remove(atividade);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
